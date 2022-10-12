56815637d2daab773c2f06cadab28a3639993589
ColterLong <ColterLong@protonmail.com>
2022-10-12 18:28:40 -0500

    clicking x or o will change background and image color of both

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -1,22 +1,33 @@
-import logo from './img/both-shapes.svg'
-import circle from './img/Oval.svg'
-import circleDark from './img/Oval-Dark.svg'
-import xShape from './img/x-shape.svg'
-import xShapeDark from './img/x-shape-dark.svg'
+import logo from './img/both-shapes.svg';
+// import React from 'react';
+import { useState } from 'react';
+import { ReactComponent as CircleShape } from './img/Oval.svg';
+import { ReactComponent as XShape } from './img/x-shape.svg';
+
 
 const StartMenu = () => {
+  const [xSelected,setXSelected] = useState(true);
+  const [circleSelected,setCircleSelected] = useState(false);
+
+  const switchSelected = function() {
+    xSelected ? setXSelected(false) : setXSelected(true);
+    circleSelected ? setCircleSelected(false) : setCircleSelected(true);
+  }
+  
   return (
     <div className='start-menu'>
       <img src={logo} alt="" className='logo' />
       <div className='pick-player'>
         <h1 className='font-xs pick-player-text'>PICK PLAYER 1's MARK</h1>
         <div className="shape-container">
-          <div className="shape-box">
-            <img src={xShape} alt="" className='shape'/>
-          </div>
-          <div className="shape-box">
-            <img src={circle} alt="" className='shape'/>
-          </div>
+          <button onClick={() => switchSelected()}
+                  className={xSelected ? "shape-btn" : "shape-btn-dark"}>
+            <XShape className={xSelected ? "shape dark-blue" : "shape silver"}/>
+          </button>
+          <button onClick={() => switchSelected()}
+                  className={circleSelected ? "shape-btn" : "shape-btn-dark"}>
+            <CircleShape className={circleSelected ? "shape dark-blue" : "shape silver"}/>
+          </button>
         </div>
         <h1 className='font-body pick-player-text'>REMEMBER : X GOES FIRST</h1>
       </div>

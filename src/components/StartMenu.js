7236b43d12885128d06ccfa6e1314fd5b05b37fc
ColterLong <ColterLong@protonmail.com>
2022-10-11 21:59:02 -0500

    added more css and buttons to start menu

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -1,22 +1,28 @@
-import logo from './Group-2-2.svg'
+import logo from './img/both-shapes.svg'
+import circle from './img/Oval.svg'
+import circleDark from './img/Oval-Dark.svg'
+import xShape from './img/x-shape.svg'
+import xShapeDark from './img/x-shape-dark.svg'
 
 const StartMenu = () => {
   return (
     <div className='start-menu'>
-      {/* <img src="Group-2-2.svg" alt=""  /> */}
       <img src={logo} alt="" className='logo' />
       <div className='pick-player'>
-        <h1 className='font-xs'>PICK PLAYER 1's MARK</h1>
-        {/* img */}
-        <h1 className='font-body'>REMEMBER: X GOES FIRST</h1>
+        <h1 className='font-xs pick-player-text'>PICK PLAYER 1's MARK</h1>
+        <div className="shape-container">
+          <div className="shape-box">
+            <img src={xShape} alt="" className='shape'/>
+          </div>
+          <div className="shape-box">
+            <img src={circle} alt="" className='shape'/>
+          </div>
+        </div>
+        <h1 className='font-body pick-player-text'>REMEMBER : X GOES FIRST</h1>
       </div>
       <div className='btns'>
-        <button className='orange btn'>
-          <h1>NEW GAME (VS CPU)</h1>
-        </button>
-        <button className="blue btn">
-          <h1>NEW GAME (VS PLAYER)</h1>
-        </button>
+        <button className='orange btn font-s'>NEW GAME (VS CPU)</button>
+        <button className="blue btn font-s">NEW GAME (VS PLAYER)</button>
       </div>
     </div>
   )

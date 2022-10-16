52618556072d7cc16023d640d7d2b5a40d5c8368
ColterLong <ColterLong@protonmail.com>
2022-10-15 20:15:34 -0500

    started useState in GameMenu and added GameContent

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -3,9 +3,25 @@ import { ReactComponent as CircleShape } from './img/Oval.svg';
 import { ReactComponent as XShape } from './img/x-shape.svg';
 import { ReactComponent as MainMenu } from './img/MainMenu.svg';
 import Button from './Button.js'
+import GameContent from './GameContent';
+import { useState } from 'react';
 
+
+// change MainMenu to redo button
+
+// global disable button for game end?
+// useEffect for who wins?
+
+// state variable of array, update array
 
 const GameMenu = ( {onSwitchMenu, showUserSelection, onSwitchUser, currentPlayer, onSwitchCurrentPlayer} ) => {
+  const [scoreboard,setScoreboard] = useState([
+    0,0,0,
+    0,0,0,
+    0,0,0
+  ])
+  
+  
   return (
     <div className='game-menu'>
       <div className="game-header">
@@ -21,29 +37,31 @@ const GameMenu = ( {onSwitchMenu, showUserSelection, onSwitchUser, currentPlayer
           <MainMenu className='shape dark-blue' onClick={() => onSwitchMenu()}/>
         </button>
       </div>
-        <div className="game-content">
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-          <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
-        </div>
+      <div className="game-content">
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+        <Button currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer}/>
+      </div>
+      {/* <GameContent currentPlayer={currentPlayer} onSwitchCurrentPlayer={onSwitchCurrentPlayer} /> */}
 
-        <div className="game-score">
-          <button className="btn">
-            X
-          </button>
-          <button className="btn">
-            Ties
-          </button>
-          <button className="btn">
-            O
-          </button>
-        </div>   
+
+      <div className="game-score">
+        <button className="btn">
+          X
+        </button>
+        <button className="btn">
+          Ties
+        </button>
+        <button className="btn">
+          O
+        </button>
+      </div>   
     </div>
   )
 }
